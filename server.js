6ba4788e28c51df5a145c18c35c70696a9ba3ec1
Saulo Carvalho <sfcarvalho@gmail.com>
2023-03-15 15:44:47 -0300

    Updating sample with db connection

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+
+const connection = mysql.createConnection({
+  host: 'mysql-container',
+  user: 'root',
+  password: 'docker',
+  database: 'docker'
+});
+
+connection.connect();
+
+app.get('/products', function(req, res) {
+   connection.query('SELECT * FROM products', function (error, results) {
+      if (error) { 
+         throw error
+   };
+   res.send(results.map(item => ({ name: item.name, price: item.price })));
+  });
+});
+
+
+app.listen(3000, function() {
+    console.log('Listening on port 3000');
+})
