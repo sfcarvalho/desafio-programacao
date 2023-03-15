47b88edfae3b13354c6da4972b528df665385d69
Saulo Carvalho <sfcarvalho@gmail.com>
2023-03-15 15:58:48 -0300

    File upload

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
+var multer  =   require('multer');  
 const mysql = require('mysql');
 const app = express();
+
+var storage =   multer.diskStorage({  
+    destination: function (req, file, callback) {  
+      callback(null, './uploads');  
+    },  
+    filename: function (req, file, callback) {  
+      callback(null, file.originalname);  
+    }  
+  });  
+
+var upload = multer({ storage : storage}).single('myfile');  
 
 const connection = mysql.createConnection({
   host: 'mysql-container',
@@ -20,6 +32,22 @@ app.get('/products', function(req, res) {
   });
 });
 
+app.get('/home', function(req, res) {
+    res.send("Home");
+});
+
+app.get('/',function(req,res){  
+    res.sendFile(__dirname + "/index.html");  
+});  
+
+app.post('/uploadjavatpoint',function(req,res){  
+    upload(req,res,function(err) {  
+        if(err) {  
+            return res.end("Error uploading file.");  
+        }  
+        res.end("File is uploaded successfully!");  
+    });  
+});  
 
 app.listen(3000, function() {
     console.log('Listening on port 3000');
